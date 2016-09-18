093a478db1d7cb64977b6b2d6e34fb823f0e982b
chomado <chomado@gmail.com>
2016-09-18 17:25:55 +0900

    Add Entity Framework & define DB model

---
+++ WebApplication160917/Models/ChomadoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication160917.Models
+{
+    public class ChomadoContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
